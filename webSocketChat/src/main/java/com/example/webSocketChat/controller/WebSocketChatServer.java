989288baf39e9a2a8636f4ce67aa895d1c255314
Felix Huang <fhuang726@gmail.com>
2019-10-10 10:50:54 -0700

    Add files via upload

---
+++ webSocketChat/src/main/java/com/example/webSocketChat/controller/WebSocketChatServer.java
@@ -0,0 +1,101 @@
+package com.example.webSocketChat.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.webSocketChat.model.Message;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap();
+    private static Map<String, String> sessionUser = new ConcurrentHashMap();
+
+    private static void sendMessageToAll(String msg) { // msg is a JSON string of the current message
+        //TODO: add send message method.
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        //TODO: add on open connection.
+        onlineSessions.put(session.getId(), session);
+
+        // username is not available in onOpen(). So when a new user David enters
+        // the chat room, the first message "David: online" is sent in WebSocket
+        // constructor and processed in onMessage with message.type ENTER.
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) throws IOException {
+        //TODO: add send message.
+
+        Message message = JSON.parseObject(jsonStr, Message.class);
+        if (message.getType().equals(Message.ENTER)) {
+            sendMessageToAll(Message.jsonStr(message.getUsername(), message.getMessage(), Message.ENTER,
+                    "" + onlineSessions.size()));
+            return;
+        }
+
+        sessionUser.put(session.getId(), message.getUsername());
+        // onlineCount is assigned here on the server side and is distributed to all current clients.
+        sendMessageToAll(Message.jsonStr(message.getUsername(), message.getMessage(), message.getType(),
+                "" + onlineSessions.size()));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        //TODO: add close connection.
+        /*
+          System.out.println("session=" + session.getId() + "  size of hash map=" + onlineSessions.size() +
+                "  size of sessionUser=" + sessionUser.size());
+         */
+        onlineSessions.remove(session.getId());
+        if (sessionUser.containsKey(session.getId())) {
+            String user = sessionUser.get(session.getId());
+            sendMessageToAll(Message.jsonStr(user, "offline", Message.QUIT, "" + onlineSessions.size()));
+            sessionUser.remove(session.getId());
+        }
+        session.close();
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
